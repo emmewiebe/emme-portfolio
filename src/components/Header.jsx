fdc2db96cbc0ca51b181cbd734b08a5215b5dc65
emmewiebe <emmewiebe@gmail.com>
2024-12-02 15:04:26 -0800

    Updated Header and Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Socials from './Socials.jsx';
-import Logo from '../img/header/logo.png'; // Ensure this path is correct
+import Logo from '../img/header/logo.png';
 import MobileNav from './MobileNav.jsx';
 
 const Header = () => {
   return (
-    <header className="fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center">
-      <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
+    <header className="fixed w-full px-[15px] lg:px-[50px] z-50 h-[80px] lg:h-[100px] flex items-center bg-white shadow-md">
+      <div className="flex flex-row items-center w-full justify-between">
         {/* Logo */}
-        <Link to="/" className="max-w-[200px]">
-          <img src={Logo} alt="Company Logo" />
+        <Link to="/" className="max-w-[100px]">
+          <img
+            src={Logo}
+            alt="Company Logo"
+            className="w-[80px] lg:w-[100px]" // Smaller logo size
+          />
         </Link>
-        {/* Navigation: initially hidden - show on desktop mode */}
-        <nav className="hidden lg:flex gap-x-12 font-semibold">
-          <Link to="/" className="text-[#696c6d] hover:text-primary transition">
+
+        {/* Navigation: Hidden on mobile, visible on desktop */}
+        <nav className="hidden lg:flex gap-x-12 font-bold text-lg">
+          <Link
+            to="/"
+            className="text-[#333] hover:text-primary transition-colors duration-300"
+          >
             Home
           </Link>
-          <Link to="/about" className="text-[#696c6d] hover:text-primary transition">
+          <Link
+            to="/about"
+            className="text-[#333] hover:text-primary transition-colors duration-300"
+          >
             About
           </Link>
-          <Link to="/portfolio" className="text-[#696c6d] hover:text-primary transition">
+          <Link
+            to="/portfolio"
+            className="text-[#333] hover:text-primary transition-colors duration-300"
+          >
             Portfolio
           </Link>
-          <Link to="/contact" className="text-[#696c6d] hover:text-primary transition">
+          <Link
+            to="/contact"
+            className="text-[#333] hover:text-primary transition-colors duration-300"
+          >
             Contact
           </Link>
         </nav>
       </div>
+
       {/* Socials */}
       <Socials />
+
       {/* Mobile Navigation */}
       <MobileNav />
     </header>
