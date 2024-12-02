2ee93bb30c7f75a7db6bec3af7e9e2b7b9e70f9e
emmewiebe <emmewiebe@gmail.com>
2024-12-02 14:50:40 -0800

    Test

--- src/components/AnimRoutes.jsx
+++ src/components/AnimRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 
 // Import Pages
@@ -7,23 +7,17 @@ import Home from '../pages/Home';
 import About from '../pages/About';
 import Portfolio from '../pages/Portfolio';
 import Contact from '../pages/Contact';
-import Project1 from '../pages/Project1';
-import Project2 from '../pages/Project2';
-import Project3 from '../pages/Project3';
-import Project4 from '../pages/Project4';
 
 const AnimRoutes = () => {
+  const location = useLocation();
+
   return (
-    <AnimatePresence initial={false} mode="wait">
-      <Routes>
+    <AnimatePresence initial={true} mode="wait">
+      <Routes key={location.pathname} location={location}>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/project-1" element={<Project1 />} />
-        <Route path="/project-2" element={<Project2 />} />
-        <Route path="/project-3" element={<Project3 />} />
-        <Route path="/project-4" element={<Project4 />} />
       </Routes>
     </AnimatePresence>
   );
