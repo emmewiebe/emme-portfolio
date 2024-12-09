4055fa836676b4c38a15f6046caf224267511e9c
emmewiebe <emmewiebe@gmail.com>
2024-12-09 10:04:40 -0800

    footer and linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="text-center py-4 bg-gray-100">
-    <p className="text-sm text-gray-500">&copy; 2024 Emme Drew. All rights reserved.</p>
+    <p className="text-sm text-gray-500">&copy; 2024 Emme Drew. All rights reserved. emersonwiebe@gmail.com</p>
   </footer>
 );
 
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,33 +1,25 @@
 import React from 'react';
 
 // Importing icons for social media links
-import { ImFacebook, ImTwitter, ImPinterest, ImInstagram, ImYoutube } from 'react-icons/im';
+import { ImLinkedin, ImPinterest, ImInstagram, ImYoutube } from 'react-icons/im';
 
 const Socials = () => {
   return (
     <div className="hidden xl:flex ml-24"> {/* Only show on larger screens */}
       <ul className="flex gap-x-4"> {/* Creates a horizontal row of social icons */}
-        {/* Facebook */}
-        <li>
-          <a href="http://www.facebook.com" target="_blank" rel="noopener noreferrer">
-            {/* Opens Facebook in a new tab */}
-            <ImFacebook /> {/* Facebook icon */}
-          </a>
-        </li>
-
-        {/* Twitter */}
-        <li>
-          <a href="http://www.twitter.com" target="_blank" rel="noopener noreferrer">
-            {/* Opens Twitter in a new tab */}
-            <ImTwitter /> {/* Twitter icon */}
-          </a>
-        </li>
-
         {/* Instagram */}
         <li>
           <a href="http://www.instagram.com" target="_blank" rel="noopener noreferrer">
             {/* Opens Instagram in a new tab */}
             <ImInstagram /> {/* Instagram icon */}
+          </a>
+        </li>
+
+        {/* LinkedIn */}
+        <li>
+          <a href="http://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+            {/* Opens LinkedIn in a new tab */}
+            <ImLinkedin /> {/* LinkedIn icon */}
           </a>
         </li>
 
