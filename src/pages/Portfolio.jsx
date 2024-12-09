7a9f6aa621d208eece4a1cfaefa644df1804d83e
emmewiebe <emmewiebe@gmail.com>
2024-12-09 10:14:19 -0800

    -

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -10,7 +10,7 @@ import covecoffee from '../img/portfolio/covecoffee.png';
 import tnthay from '../img/portfolio/tnthay.png';
 
 // Import your poster images for the conveyor belt
-import poster1 from '../img/portfolio/olderposterjpg.png';
+import poster1 from '../img/portfolio/olderposter.jpg';
 import poster2 from '../img/portfolio/gameday.png';
 import poster3 from '../img/about/cove-logo.png';
 import poster4 from '../img/about/cove-mockup5.png';
